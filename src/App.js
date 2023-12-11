7d7a9ff8d0b2003c8b8b79b1dc3f2ff048a598f1
kamara Abdulrahman <rahmankamara@icloud.com>
2023-12-11 14:58:07 +0100

    modified for CI

--- src/App.js
+++ src/App.js
@@ -1,61 +1,51 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import "./App.css";
 
 import NewExpense from "./components/NewExpense/NewExpense";
 import Expenses from "./components/Expenses/Expenses";
 
 const Dummy_Expenses = [
-    {
-      id: "1",
-      title: "House",
-      amount: "$435.54",
-      date: new Date(2021, 7, 12),
-    },
-    {
-      id: "11",
-      title: "Bred",
-      amount: "$435.4",
-      date: new Date(2021, 3, 12),
-    },
-    {
-      id: "2",
-      title: "Shop",
-      amount: "$43.54",
-      date: new Date(2021, 2, 12),
-    },
-    {
-      id: "6",
-      title: "Money",
-      amount: "$435.88",
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  {
+    id: "1",
+    title: "House",
+    amount: "$435.54",
+    date: new Date(2021, 7, 12),
+  },
+  {
+    id: "11",
+    title: "Bred",
+    amount: "$435.4",
+    date: new Date(2021, 3, 12),
+  },
+  {
+    id: "2",
+    title: "Shop",
+    amount: "$43.54",
+    date: new Date(2021, 2, 12),
+  },
+  {
+    id: "6",
+    title: "Money",
+    amount: "$435.88",
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
-
   const [expenses, setExpenses] = useState(Dummy_Expenses);
-  
-const addExpense =(expense) => {
-  //we get our prevexpenses by react
 
-setExpenses((prevExpenses) => {
-  return [expense, ...prevExpenses]
-})
-}
+  const addExpense = (expense) => {
+    //we get our prevexpenses by react
 
-  // react without JSX
-
-  // return React.createElement('div', {}, 
-  // React.createElement('h2', {}, 'React App' ),
-  // React.createElement(Expenses, {product: expenses})
-  // )
-
-  // react with JSX
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
 
   return (
     <div>
-     <NewExpense onAddExpense={addExpense}/>
-      <Expenses product={expenses}/>
+      <NewExpense onAddExpense={addExpense} />
+      <Expenses product={expenses} />
     </div>
   );
 }
